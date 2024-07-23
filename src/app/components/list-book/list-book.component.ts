8c4671aa57f9d866f6c381b7d887e2ae33ef93a7
nguphapthayduc <nguphapthayduc@gmail.com>
2024-07-23 18:22:47 +0700

    Update UI and responsive layout

--- src/app/components/list-book/list-book.component.ts
+++ src/app/components/list-book/list-book.component.ts
@@ -28,7 +28,7 @@ import { CartService } from 'src/app/cart.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { BehaviorSubject, Subject } from 'rxjs';
 import { LoginService } from 'src/app/login.service';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { AuthService } from 'src/app/auth.service';
 @Component({
   selector: 'app-list-book',
@@ -37,7 +37,7 @@ import { AuthService } from 'src/app/auth.service';
 })
 export class ListBookComponent implements OnInit, AfterViewInit {
 
-  renderer= inject(Renderer2);
+  renderer = inject(Renderer2);
 
   bookService = inject(BookService);
 
@@ -53,9 +53,9 @@ export class ListBookComponent implements OnInit, AfterViewInit {
 
   authService = inject(AuthService);
 
-  isrole : boolean = false;
+  isrole: boolean = false;
 
-  @Input() pageSize = 4;
+  @Input() pageSize = 12;
 
   @Input() totalBooks = 0;
 
@@ -68,6 +68,10 @@ export class ListBookComponent implements OnInit, AfterViewInit {
   genreService = inject(GenreService);
 
   bookGenresService = inject(BookGenresService);
+
+  idGenreSubject = new Subject<number>();
+
+  idgenrebylist: number = 0;
 
   @Input() bookSubject = new BehaviorSubject<any>(null);
 
@@ -92,16 +96,23 @@ export class ListBookComponent implements OnInit, AfterViewInit {
   sortedData: Book[] = [];
 
   toggleFiller() {
+
     this.showFiller = !this.showFiller;
+
   }
 
   readonly dialog = inject(MatDialog);
 
   sortData(sort: Sort) {
+
     debugger
+
     const data = this.books.slice();
+
     if (!sort.active || sort.direction === '') {
+
       this.sortedData = data;
+
       this.updatePagedBooks();
 
 
@@ -109,34 +120,53 @@ export class ListBookComponent implements OnInit, AfterViewInit {
     }
 
     this.sortedData = data.sort((a, b) => {
+
       const isAsc = sort.direction === 'asc';
+
       switch (sort.active) {
+
         case 'price':
+
           return compare(a.price, b.price, isAsc);
+
         case 'quantity':
+
           return compare(a.stock_quantity, b.stock_quantity, isAsc);
+
         default:
+
           return 0;
       }
     });
+
     console.log(this.sortedData);
+
     this.books = this.sortedData;
+
     this.updatePagedBooks();
+
     function compare(a: number | string, b: number | string, isAsc: boolean) {
+
       return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+
     }
 
   }
 
   openDialog(book: Book): void {
+
     const dialogRef = this.dialog.open(DialogAnimationsExampleDialogComponent, {
+
       width: '250px',
+
       data: { book }
+
     });
 
     dialogRef.afterClosed().subscribe(result => {
 
       if (result === 'confirm') {
+
         this.handleDelete(book.bookID);
 
 
@@ -149,53 +179,78 @@ export class ListBookComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
+
     this.checkRole();
+
     this.data.currentMessage.subscribe(message => {
+
       this.message = message;
+
       if (message) {
+
         this.searchBook(message);
+
         this.getGenre()
+
       } else {
+
         this.loadBooks();
+
       }
     });
 
 
     this.loginService.getUser().subscribe({
+
       next: (user) => {
+
         if (user) {
+
           this.idUser = user.userID;
 
         } else {
+
           console.log("get idUser fale");
 
         }
       }, error: (error) => {
+
         console.error("Error fetching user:", error);
+
       }
     })
 
     this.route.params.subscribe((param) => {
+
       this.genreId = param['id']
+
       if (this.genreId) {
 
 
         this.bookService.getBookbyGenre(this.genreId).subscribe({
+
           next: (data) => {
+
             this.books = data;
+
             this.totalBooks = data.length;
+
             this.updatePagedBooks();
+
             this.getGenre()
+
           }
 
         })
       } else {
         this.bookService.getAll().subscribe({
+
           next: (data) => {
 
             this.books = data;
 
             this.books.forEach((a: any) => {
+
               Object.assign(a, { quantity: 1, total: a.price });
             })
 
@@ -211,57 +266,101 @@ export class ListBookComponent implements OnInit, AfterViewInit {
   }
 
 
-  checkRole(){
+  checkRole() {
+
     let role = this.authService.getRoles();
+
     this.isrole = role.includes('Admin');
+
   }
 
   addTocart(book: Book) {
 
     this.bookSubject.next(book);
 
-    this.cartService.addCart(book.bookID ,this.idUser).subscribe({
-      next:(value)=> {
-        this.cartService.loadCart();  
-        console.log("add to cart sucssec", value);
-      }, error:(err)=> {
-        this.cartService.openSnackBar();
-        console.log("add to cart fale" ,err);
+    this.cartService.addCart(book.bookID, this.idUser).subscribe({
 
-      },     
+      next: (value) => {
+
+        this.cartService.loadCart();
+
+        console.log("add to cart sucssec", value);
+
+      }, error: (err) => {
+
+        this.cartService.openSnackBar();
+
+        console.log("add to cart fale", err);
+
+
+      },
     })
 
   }
 
+  getIDgenre(id: number) {
 
-  getGenre() {
-    this.genreService.getAll().subscribe({
+    this.idGenreSubject.next(id);
+
+    this.idGenreSubject.subscribe({
+
       next: (data) => {
+
+        this.idgenrebylist = data
+
+        console.log(222222, data);
+
+
+      }
+    })
+    console.log(111111111, this.idgenrebylist);
+
+  }
+  getGenre() {
+
+    this.genreService.getAll().subscribe({
+
+      next: (data) => {
+
         this.genres = data;
-        
+
+
         this.mapGenresToBooks();
       },
       error: (e) => {
+
         console.error('Error fetching genres:', e);
+
       }
     });
   }
   mapGenresToBooks(): void {
+
     this.books.forEach(book => {
+
       book.genreName = this.genres.find(genre => genre.genreID === book.genreID)?.name || 'Unknown';
+    
     });
   }
 
 
   searchBook(query: string) {
+
     this.bookService.searchBook(query).subscribe({
+
       next: (data) => {
+
         this.books = data;
+
         this.totalBooks = data.length;
+
         this.updatePagedBooks();
+
       },
       error: (e) => {
+
         console.log(e);
+
       }
     });
   }
@@ -269,27 +368,42 @@ export class ListBookComponent implements OnInit, AfterViewInit {
   loadBooks() {
     // Load all books
     this.bookService.getAll().subscribe({
+
       next: (data) => {
+
         this.books = data;
+
         this.totalBooks = data.length;
+
         this.updatePagedBooks();
+
       },
       error: (e) => {
+
         console.log(e);
+
       }
     });
   }
   handleDelete(id: number) {
+
     this.bookService.deleteBook(id).subscribe({
+
       next: () => {
+
         console.log("Delete Suces");
+
         this.books = this.books.filter(book => book.bookID !== id);
-        this.loadBooks()
+
+        this.loadBooks();
+
         this.router.navigate(['book/list']);
+
         window.location.reload();
 
       },
       error: (e) => {
+
         console.log(e);
 
       }
@@ -298,18 +412,27 @@ export class ListBookComponent implements OnInit, AfterViewInit {
   }
 
   onPageChange(event: PageEvent) {
+
     this.pageSize = event.pageSize;
+
     this.currentPage = event.pageIndex;
+
     this.updatePagedBooks();
+
   }
   updatePagedBooks() {
+
     const startIndex = this.currentPage * this.pageSize;
+
     const endIndex = startIndex + this.pageSize;
+
     this.pagedBooks = this.books.slice(startIndex, endIndex);
+
   }
   getImageUrl(imagePath: string): string {
 
     const fakePathPrefix = 'C:\\fakepath\\';
+
     if (imagePath && imagePath.startsWith(fakePathPrefix)) {
 
       return `${imagePath.substring(fakePathPrefix.length)}`;
@@ -331,15 +454,24 @@ export class ListBookComponent implements OnInit, AfterViewInit {
   // }
 
 
-   drop(event: CdkDragDrop<Book[]>) {
+  drop(event: CdkDragDrop<Book[]>) {
+
     if (event.previousContainer === event.container) {
+
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+   
     } else {
+
       transferArrayItem(
+
         event.previousContainer.data,
+
         event.container.data,
+
         event.previousIndex,
+
         event.currentIndex
+
       );
     }
   }
@@ -347,11 +479,14 @@ export class ListBookComponent implements OnInit, AfterViewInit {
 
 
   onFilterClick(event: Event) {
+
     const target = event.currentTarget as HTMLElement;
 
     // Remove active class from all filters
     const filters = document.querySelectorAll('.filter');
+    
     filters.forEach(filter => {
+     
       this.renderer.removeClass(filter, 'active');
     });
 
